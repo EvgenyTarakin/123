7f36a2a1027ed18650d2948117f9fd63a2338633
EvgeniiTarakin <luzju99@mai.ru>
2021-03-22 20:10:32 +0700

    1.1

---
+++ 12/AllViewControllers/UserAuthorizationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UserAuthorizationViewController.swift
+//  12
+//
+//  Created by Евгений Таракин on 04.04.2021.
+//
+
+import UIKit
+
+class UserAuthorizationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
