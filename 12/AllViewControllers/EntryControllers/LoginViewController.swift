4525110a758630d8f8db965ee3f3a969b0b053c6
EvgeniiTarakin <luzju99@mai.ru>
2021-04-14 23:08:40 +0700

    3

--- 12/AllViewControllers/EntryControllers/LoginViewController.swift
+++ 12/AllViewControllers/EntryControllers/LoginViewController.swift
@@ -10,52 +10,6 @@ import UIKit
 class LoginViewController: UIViewController, UITextFieldDelegate {
 
     
-    @IBOutlet weak var authorizationLabel: UILabel!
-    @IBOutlet weak var instructionLabel: UILabel!
-    
-    @IBOutlet weak var loginTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    
-    @IBOutlet weak var entryButton: UIButton!
-    @IBOutlet weak var registrationButton: UIButton!
-    
-    @IBAction func tappedRegistrationButton(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let controller = storyboard.instantiateViewController(identifier: "RegistrationViewController")
-        navigationController?.pushViewController(controller, animated: true)
-    }
-    
-    
-    private lazy var tapViewGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapView))
-    @objc func tapView() {
-        loginTextField.resignFirstResponder()
-        passwordTextField.resignFirstResponder()
-        moveObjects(1)
-    }
-    
-    func moveObjects(_ alpha: CGFloat) {
-        authorizationLabel.alpha = alpha
-        instructionLabel.alpha = alpha
-        entryButton.alpha = alpha
-        registrationButton.alpha = alpha
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        moveObjects(0)
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == loginTextField {
-            textField.resignFirstResponder()
-            passwordTextField.becomeFirstResponder()
-        } else if textField == passwordTextField {
-            passwordTextField.resignFirstResponder()
-            moveObjects(1)
-        }
-        return false
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -67,10 +21,48 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         loginTextField.delegate = self
         passwordTextField.delegate = self
         
+        let tapViewGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapView))
+        
         view.addGestureRecognizer(tapViewGestureRecognizer)
     }
-
-
+    
+    
+    @IBOutlet weak var authorizationLabel: UILabel!
+    @IBOutlet weak var instructionLabel: UILabel!
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var entryButton: UIButton!
+    @IBOutlet weak var registrationButton: UIButton!
+    
+    
+    @IBAction func tappedEntryButton(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(identifier: "TabBarController")
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    
+    @IBAction func tappedRegistrationButton(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(identifier: "RegistrationViewController")
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    
+    @objc func tapView() {
+        view.endEditing(true)
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == loginTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            passwordTextField.resignFirstResponder()
+        }
+        return false
+    }
+    
 }
-
-
